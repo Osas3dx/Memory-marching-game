d16d83671be713b76dc929d82a91864a41f501fd
Osas3dx <oboseosayomore37@gmail.com>
2023-01-15 02:21:59 +0800

    the menubar for mobile phones

---
+++ app.js
@@ -0,0 +1,16 @@
+const menuSlide = () =>{
+    const burger = document.querySelector(".burger");
+    const  menu = document.querySelector(".menu");
+    const menuLinks = document.querySelectorAll(".menu ul li")
+    burger.addEventListener('click', () =>{
+        menu.classList.toggle('menu-active')
+        menuLinks.forEach((link, index) => {
+            link.style.animation = `menufade 0.5s ease forwards ${index / 7+ 0}s`
+            console.log(index/7);
+            
+        });
+    })
+
+
+}
+menuSlide();
