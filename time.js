757253d4b16dc2f8a6f97a1352960a3613909995
Osas3dx <oboseosayomore37@gmail.com>
2023-01-22 17:33:19 +0800

    worked on the about and home too 4

--- time.js
+++ time.js
@@ -3,20 +3,20 @@ function showTime(){
     let today = new Date();
     let hourNow = today.getHours();
     let minNow = today.getMinutes();
-    let secNow = today.getSeconds();
+    //let secNow = today.getSeconds();
     let greeting= document.querySelector('.date')
     
-    const amPm = hourNow >= 12 ?'PM' : 'AM';
-    hourNow = hourNow % 12 || 12;
+    //const amPm = hourNow >= 12 ?'PM' : 'AM';
+    
     function addZero(n){
         return (parseInt(n, 10) < 10 ? '0' : '')+n;
     }
     if (hourNow <= 12)  {
-        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm} <br>Good Morning`
-    } else if(hourNow < 12){
-        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm} <br>Good Afternoon` 
+        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm}`
+    } else if(hourNow < 18){
+        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm}` 
     } else{
-        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm} <br>Good Evening`
+        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm} `
     }
     setTimeout(showTime, 1000);
 }
