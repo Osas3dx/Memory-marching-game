9494b461247cdc338aa6bac1b3d7ef9c13622dec
Osas3dx <oboseosayomore37@gmail.com>
2023-01-22 20:47:04 +0800

    working on the time function

--- time.js
+++ time.js
@@ -5,16 +5,18 @@ function showTime(){
     let minNow = today.getMinutes();
     let secNow = today.getSeconds();
     let greeting= document.querySelector('.date')
-
+    
+    const amPm = hourNow >= 12 ?'PM' : 'AM';
+    hourNow = hourNow % 12 || 12;
     function addZero(n){
         return (parseInt(n, 10) < 10 ? '0' : '')+n;
     }
     if (hourNow <= 12)  {
-        greeting.innerHTML = hourNow +":" + addZero(minNow)+":"+addZero(secNow)+ " Good Morning"
+        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm} <br>Good Morning`
     } else if(hourNow < 18){
-        greeting.innerHTML = hourNow +":" + addZero(minNow)+":"+addZero(secNow)+ " Afternoon"        
+        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm} <br>Good Afternoon` 
     } else{
-        greeting.innerHTML = hourNow +":" + addZero(minNow)+":"+addZero(secNow)+ " Good Evening"
+        greeting.innerHTML =`${hourNow}:${addZero(minNow)}:${addZero(secNow)} ${amPm} <br>Good Evening`
     }
     setTimeout(showTime, 1000);
 }
