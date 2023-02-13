868490d52e5d0aadc6d3e8c380b4f4342169996b
Osas3dx <oboseosayomore37@gmail.com>
2023-02-14 02:14:44 +0800

    made some update to the property's page

--- properties/properties.js
+++ properties/properties.js
@@ -1,5 +1,23 @@
-let cards = document.querySelectorAll("section ul li")
+const module = document.querySelector(".module");
+const smallsizes = document.querySelectorAll("section ul li img");
+const bigsize = document.querySelector('.full-img');
+const imgText = document.querySelector('.caption');
 
+smallsizes.forEach(smallsize =>{
+    smallsize.addEventListener("click", ()=>{
+        module.classList.add("open");
+        bigsize.classList.add("open");
+        const smallsizespic = smallsize.getAttribute("data-original");
+        bigsize.src = smallsizespic;
 
-
-cards.forEach({})
+        const text = smallsize.alt;
+        imgText.textContent = text
+        console.log(smallsize.alt)
+    })
+})
+module.addEventListener("click", (e) =>{
+    if (e.target.classList.contains('module')){
+        module.classList.remove('open');
+        bigsize.classList.remove("open");
+    }
+})
