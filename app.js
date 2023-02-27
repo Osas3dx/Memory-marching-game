52c964d5f4e957484573baccd1581b020cc6bcc2
Osas3dx <oboseosayomore37@gmail.com>
2023-02-27 11:55:16 +0800

    saving a new form file as calculator

--- app.js
+++ app.js
@@ -29,4 +29,38 @@ const menuSlide = () =>{
 //showTime();
 
 menuSlide();
+let btn = document.querySelectorAll("#button")
+let hostelLists = document.querySelectorAll('.universtiy')
+let hostelListEls = document.querySelectorAll(".randomInfor li")
+let boxELs = document.querySelector('.li-box')
+let module = document.querySelector('.modul-box')
+let textHostle = document.querySelector('.textHostels')
 
+
+
+hostelLists.forEach( hostelList =>{
+    hostelList.addEventListener("click", (e)=>{
+    module.style.display = "block"
+    hostelListEls.forEach( hostelListEl =>{
+        console.log(hostelListEl.childNodes[2])
+    })
+    let tarlist = e.target.id 
+    if(hostelLists.length === hostelListEl.length){
+        console.log(`W ${hostelLists.length} and ${hostelListEl.length}`)
+    }
+    console.log(hostelListEl.length)
+    boxELs.textContent = tarlist + tarlist
+    tarlist.addEventListener("click", (e)=>{
+        console.log('what is the hell')
+    })
+        
+
+    })
+})
+
+function fib(n){
+    if(n <= 2) return 1;
+    return fib(n -1) + fib(n-2);
+}
+
+console.log(fib(1))
--- properties/properties.js
+++ properties/properties.js
@@ -3,13 +3,23 @@ const smallsizes = document.querySelectorAll("section ul li img");
 const bigsize = document.querySelector('.full-img');
 const imgText = document.querySelector('.caption');
 
+
+smallsizes.forEach(smallsize =>{
+    smallsize.addEventListener("load", ()=>{    
+        let div = document.createElement("div");
+        let addclass = div.classList;
+        addclass.add("proImg")
+        div.appendChild(smallsize)
+        console.log("i'm here")
+    })
+})
+
 smallsizes.forEach(smallsize =>{
     smallsize.addEventListener("click", ()=>{
         module.classList.add("open");
         bigsize.classList.add("open");
         const smallsizespic = smallsize.getAttribute("data-original");
         bigsize.src = smallsizespic;
-
         const text = smallsize.alt;
         imgText.textContent = text
         console.log(smallsize.alt)
